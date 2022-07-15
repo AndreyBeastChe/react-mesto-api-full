8309908e05cba69260ea95d6c7328ad949da80fe
a-chechetkin <a-chechetkin@yandex-team.ru>
2022-07-15 23:51:22 +0300

    fix

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,4 +1,3 @@
-// Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
   'https://vse-na-meste.nomoredomains.xyz',
   'http://vse-na-meste.nomoredomains.xyz',
@@ -6,32 +5,23 @@ const allowedCors = [
   'http://api.vse-na-meste.nomoredomains.xyz',
   'localhost:3000'
 ];
-const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
-module.exports(function(req, res, next) {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  next();
-});
-
-const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-
-
-// Если это предварительный запрос, добавляем нужные заголовки
-if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-}
-
-// сохраняем список заголовков исходного запроса
-const requestHeaders = req.headers['access-control-request-headers'];
-if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-}
+module.exports = (req, res, next) => {
+    const { origin } = req.headers;
+    const { method } = req;
+    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
+    const requestHeaders = req.headers['access-control-request-headers'];
+  
+    if (allowedCors.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+      res.header('Access-Control-Allow-Credentials', true);
+    }
+  
+    if (method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      return res.status(200);
+    }
+  
+    next();
+  };

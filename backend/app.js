5e478ddef8646928df421add99315cf3ba71ff22
a-chechetkin <a-chechetkin@yandex-team.ru>
2022-07-16 16:55:08 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -104,7 +104,7 @@ app.use((err, req, res, next) => {
   if (err.statusCode) {
     return res.status(err.statusCode).send({ message: err.message });
   }
-  return res.status(500).send({ message: 'Что-то пошло не так' + err });
+  return res.status(500).send({ message: 'Что-то пошло не так', err: err });
 });
 
 app.listen(PORT, () => {
